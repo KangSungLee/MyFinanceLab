0cd871ebbe812bc40de091c13fc83098c6241e1d
KangSungLee <rkdtjd1020@naver.com>
2024-12-30 07:24:14 +0900

    commit

---
+++ src/main/java/com/example/myFinanceLab/MyFinanceLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.myFinanceLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinanceLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFinanceLab1Application.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html; charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Stock Trading Platform</title>
+</head>
+<body>
+    <h1>Welcome to Stock Trading Platform!</h1>
+    <p>Spring Boot JSP 프로젝트가 성공적으로 실행되었습니다!</p>
+</body>
+</html>
